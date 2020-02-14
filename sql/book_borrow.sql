c4b7d06752665a79bda86a64a0c28698785ef264
ry0902 <1332149308@qq.com>
2020-02-14 11:58:42 +0800

    更新sql

--- sql/book_borrow.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `book_borrow` VALUES (1, 1, 1, '2020-1-1', '2020-2-1');
-INSERT INTO `book_borrow` VALUES (4, 2, 2, '2020-1-1', '2020-3-1');
-INSERT INTO `book_borrow` VALUES (14, 1, 5, '2020-1-2', '2020-2-2');
--- sql/user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `user` VALUES (1, 'admin', '123');
-INSERT INTO `user` VALUES (2, 'mmk', '123');
